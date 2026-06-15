efc793d6861b619328a898d1e26ddfff835538de
AsnDen <asnden@gmail.com>
2026-06-16 00:24:57 +0300

    feat: Added entry point

--- src/mediawiki_monitor/__main__.py
+++ src/mediawiki_monitor/__main__.py
@@ -1,6 +1,17 @@
-def main():
+from flask import Flask
+
+from mediawiki_monitor.routes import create_wikistat_blueprint
+
+
+def main() -> None:
     """Entry point."""
-    print("Hello from mediawiki-monitor!")
+    app = Flask(__name__)
+
+    wikistat_blueprint = create_wikistat_blueprint()
+
+    app.register_blueprint(wikistat_blueprint)
+
+    app.run()
 
 
 if __name__ == "__main__":
