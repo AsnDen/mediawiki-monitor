0dd7b7c77a818abee0aeeeb20a93e392abecc21a
AsnDen <asnden@gmail.com>
2026-06-21 17:10:33 +0300

    fix(mediawiki-api)!: Fix 404 error
    BREAKING CHANGE: Urls in `config.py` should not end with `api.php`.
    Instead they are just base url for wiki.
    
    `MediawikiAPIService` have `api_relative_url` property to specify path
    to `api.php` file.

--- src/mediawiki_monitor/service/mediawiki_api_service.py
+++ src/mediawiki_monitor/service/mediawiki_api_service.py
@@ -117,19 +117,17 @@ class AllusersResponse(TypedDict):
 
 
 class MediawikiAPIService:
-    def __init__(
-        self,
-        api_url: str,
-    ) -> None:
+    def __init__(self, api_base_url: str, api_relative_url: str = "api.php") -> None:
         self.client: Client = Client(
-            base_url=api_url,
+            base_url=api_base_url,
             timeout=10.0,
             follow_redirects=True,
         )
+        self.api_relative_url: str = api_relative_url
 
     def get_site_info(self) -> SiteinfoPayload:
         response = self.client.get(
-            "",
+            self.api_relative_url,
             params={
                 "action": "query",
                 "meta": "siteinfo",
@@ -145,7 +143,7 @@ class MediawikiAPIService:
 
     def get_statistics(self) -> StatisticsPayload:
         response = self.client.get(
-            "",
+            self.api_relative_url,
             params={
                 "action": "query",
                 "meta": "siteinfo",
@@ -162,7 +160,7 @@ class MediawikiAPIService:
 
     def get_users_by_group(self, groups: tuple[str, ...]) -> list[User]:
         response = self.client.get(
-            "",
+            self.api_relative_url,
             params={
                 "action": "query",
                 "list": "allusers",
@@ -193,7 +191,7 @@ class MediawikiAPIService:
     ) -> list[RecentChange]:
         # TODO (asnden): rerequest in case total > limit (see mediawiki api doc)
         response = self.client.get(
-            "",
+            self.api_relative_url,
             params={
                 "action": "query",
                 "list": "recentchanges",
@@ -222,7 +220,7 @@ class MediawikiAPIService:
 
     def get_diff(self, from_rev: int, to_rev: int) -> str:
         response = self.client.get(
-            "",
+            self.api_relative_url,
             params={
                 "action": "compare",
                 "fromrev": from_rev,
