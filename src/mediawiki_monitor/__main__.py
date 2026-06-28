d0c80e9b2bc2593654ea523da28d3aa12130a99b
AsnDen <asnden@gmail.com>
2026-06-28 21:53:06 +0300

    build: gunicorn dependency
    Gunicorn dependency to run flask server.
    Rewrites __main__.py to allow to use gunicorn.

--- src/mediawiki_monitor/__main__.py
+++ src/mediawiki_monitor/__main__.py
@@ -3,16 +3,20 @@ from flask import Flask
 from mediawiki_monitor.routes import create_wikistat_blueprint
 
 
-def main() -> None:
-    """Entry point."""
+def create_app() -> Flask:
+    """App factory."""
     app = Flask(__name__)
 
-    wikistat_blueprint = create_wikistat_blueprint()
+    app.register_blueprint(create_wikistat_blueprint())
 
-    app.register_blueprint(wikistat_blueprint)
+    return app
 
+
+def run_app() -> None:
+    """Runs app locally for tests."""
+    app = create_app()
     app.run(host="127.0.0.1", port=8000)
 
 
 if __name__ == "__main__":
-    main()
+    run_app()
