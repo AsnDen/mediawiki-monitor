464c26d9972624c52cb9a7d9f45297fb13061aea
AsnDen <asnden@gmail.com>
2026-06-30 20:03:50 +0300

    fix: proxy fix

--- src/mediawiki_monitor/__main__.py
+++ src/mediawiki_monitor/__main__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from werkzeug.middleware.proxy_fix import ProxyFix
 
 from mediawiki_monitor.routes import create_wikistat_blueprint
 
@@ -6,6 +7,13 @@ from mediawiki_monitor.routes import create_wikistat_blueprint
 def create_app() -> Flask:
     """App factory."""
     app = Flask(__name__)
+
+    app.wsgi_app = ProxyFix(
+        app.wsgi_app,
+        x_for=1,
+        x_proto=1,
+        x_host=1,
+    )
 
     app.register_blueprint(create_wikistat_blueprint())
 
