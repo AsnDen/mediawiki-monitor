fdc6943053d8c6aaf3eeaf35ad870c7a9fa6413c
AsnDen <asnden@gmail.com>
2026-07-01 19:49:16 +0300

    feat: new wikis

--- src/mediawiki_monitor/config.py
+++ src/mediawiki_monitor/config.py
@@ -2,5 +2,9 @@ URLS = {
     "corvax": "https://station14.ru/api.php",
     "dead_space": "https://wiki.deadspace14.net/api.php",
     "ss220": "https://wiki.ss220.club/api.php",
+    "space_stories": "https://spacestories.club/api.php",
+    "adventure_station": "https://wiki.adventurestation.ru/api.php",
+    "fire_station": "https://fire.station.wiki.shizainc.com/w/api.php",
+    "starlight": "https://wiki.starlight.network/api.php",
     "wizards": "https://wiki.spacestation14.com/api.php",
 }
